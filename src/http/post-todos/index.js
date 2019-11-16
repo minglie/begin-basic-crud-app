209306be07b72ae56e9f31548c37b3a3972a2972
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 17:02:37 +0800

    Update index.js

--- src/http/post-todos/index.js
+++ src/http/post-todos/index.js
@@ -4,15 +4,15 @@ let data = require('@begin/data')
 exports.handler = async function post (req) {
   let todo = arc.http.helpers.bodyParser(req) // Base64 decodes + parses body
   todo.created = todo.created || Date.now()
-  await data.set({
-    table: 'todos',
-    ...todo
-  })
-  return {
-    statusCode: 302,
+
+   return {
+    statusCode: 201,
     headers: {
-      'location': '/',
+      'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
+    },
+    body: JSON.stringify({
+      "data":"ok"
+    })
   }
 }
