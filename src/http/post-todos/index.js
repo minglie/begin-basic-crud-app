04ee021107f8bbf485d8ef132400db4b9fa699bf
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 19:47:45 +0800

    Update index.js

--- src/http/post-todos/index.js
+++ src/http/post-todos/index.js
@@ -1,19 +1,21 @@
 let arc = require('@architect/functions')
-
+let Db=require("./Db.js")
 
 exports.handler = async function post (req) {
    req.params = arc.http.helpers.bodyParser(req) // Base64 decodes + parses body
  
   console.log(req.params.text)
   
+  var rows= await Db.doSql(req.params.sql);
+
+   
+   
    return {
     statusCode: 201,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body: JSON.stringify({
-      "data":"ok"
-    })
+    body: JSON.stringify(rows)
   }
 }
