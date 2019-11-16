85e00041b2c9c19a8920049a79a57c2873354985
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 20:46:11 +0800

    Update index.js

--- src/http/post-todos/index.js
+++ src/http/post-todos/index.js
@@ -6,7 +6,7 @@ exports.handler = async function post (req) {
   var rows= await Db.doSql(req.params.sql);
   var result={"code":3002,"message":"操作成功","success":true,"data":rows}  
    return {
-    statusCode: 201,
+    statusCode: 200,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
